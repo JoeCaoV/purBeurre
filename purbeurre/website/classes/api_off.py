bac404ecfd73adf1b6958a73f6f757cbba3cc2aa
Joevin <Joe.Caovan@gmail.com>
2019-06-08 23:55:58 +0200

    register backend done

--- purbeurre/website/classes/api_off.py
+++ purbeurre/website/classes/api_off.py
@@ -1,4 +1,5 @@
 """Module to request the API"""
+import requests
 
 class ApiOff:
     """Contains the methods to request the OpenfoodFact Api
@@ -9,12 +10,24 @@ class ApiOff:
         self.search_url = 'https://fr-en.openfoodfacts.org/cgi/search.pl'
 
     def research_product(self, terms):
+        """Research and return the first product found
+        according to the research terms
+        """
         params = {'search_terms' : terms, 'search_simple' : 1, 'json' : 1}
         api_get = requests.get(self.search_url, params=params).json()
-        product = { 
-            'name' : products[0]['product_name_fr'],
-            'url' : [products][0]['url'],
-            'nutriscore' : [products][0]['nutriments']['nutrition-score-fr'],
-            'image_url' : [products][0]['image_front_url'],
-        }
+        try:
+            product = {'name': api_get['products'][0]['product_name_fr'],
+                       'url': api_get['products'][0]['url'],
+                       'nutriscore': api_get['products'][0]['nutriments']['nutrition-score-fr'],
+                       'image_url': api_get['products'][0]['image_front_url'],
+                       'categories': api_get['products'][0]['categories']}
+        except:
+            return False
         return product
+
+    def research_alternative(self, terms, nutriscore):
+        """Research a list of products found according to the research terms
+        only return product with a better (smaller) nutriscore  
+        """
+        pass
+
--- purbeurre/website/tests.py
+++ purbeurre/website/tests.py
@@ -1,11 +1,41 @@
 from django.test import TestCase
-from .classes import api_off
+from mock import patch
+from .classes.api_off import ApiOff
 # Create your tests here.
 
 class ApiOpenFood(TestCase):
+    """Contain the test for the OpenFoodFact Api"""
+
     def setUp(self):
         self.api = ApiOff()
 
-    def test_api(self):
-        product = self.api.research_product('nutella')
-        print(product)
+    @patch('requests.get')
+    def test_working_request(self, mock_api):
+        """Testing a wanted result"""
+        api_result = {'products' :
+                      [
+                          {
+                            'product_name_fr': 'name',
+                            'url': 'url',
+                            'image_front_url': 'image_url',
+                            'nutriments':{
+                                'nutrition-score-fr':25
+                            },
+                            'categories': 'Pate à pain'
+                          }
+                      ]
+                     }
+        mock_api.return_value.json.return_value = api_result
+        response = {'name': 'name',
+                    'url': 'url',
+                    'nutriscore': 25,
+                    'image_url': 'image_url',
+                    'categories': 'Pate à pain'}
+        self.assertEqual(response, self.api.research_product('test'))
+
+    @patch('requests.get')
+    def test_failing_request(self, mock_api):
+        """Testing a failing result"""
+        mock_api.return_value.json.return_value = 'Nope'
+        self.assertFalse(self.api.research_product('test'))
+        
