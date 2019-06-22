3f96cbc5f9772c290a9d011a12f896ae4ee8eea4
Joevin Caovan <joe.caovan@gmail.com>
2019-06-22 14:30:00 +0200

    static_root set

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['murmuring-falls-38065.herokuapp.com']
 
@@ -114,7 +114,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
