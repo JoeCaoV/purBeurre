c36721a9512920e4f23f38e3385a838e3908fb19
Joevin Caovan <joe.caovan@gmail.com>
2019-06-24 13:36:34 +0200

    Setting modified

--- purbeurre/purbeurre/prod_settings.py
+++ purbeurre/purbeurre/prod_settings.py
@@ -5,7 +5,7 @@ from .settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+MIDDLEWARE += {'whitenoise.middleware.WhiteNoiseMiddleware'}
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
