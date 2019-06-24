4ac514cd36a08ca50b40dbb5cf7d355ab6f4ff6b
Joevin Caovan <joe.caovan@gmail.com>
2019-06-24 08:13:47 +0200

    trying to fix error 500

--- purbeurre/purbeurre/prod_settings.py
+++ purbeurre/purbeurre/prod_settings.py
@@ -5,7 +5,7 @@ from .settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+MIDDLEWARE_CLASSES = ['whitenoise.middleware.WhiteNoiseMiddleware']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
